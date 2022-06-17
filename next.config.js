540f3b2d937190dbc873c1a3a5fc7c43b0c36317
Krina Wibisana <krinawibisana@gmail.com>
2022-06-17 13:16:17 +0700

    [SETUP] add several devendencies and configurations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require('path');
+const withPWA = require('next-pwa');
 
-module.exports = nextConfig
+module.exports = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    register: true,
+    sw: 'service-worker.js',
+  },
+  optimizeFonts: false,
+  images: {
+    domains: ['raw.githubusercontent.com', 'res.cloudinary.com'],
+  },
+  webpack(config, _options) {
+    config.resolve.alias['@pages'] = path.join(__dirname, 'pages');
+    config.resolve.alias['@components'] = path.join(__dirname, 'components');
+    config.resolve.alias['@utils'] = path.join(__dirname, 'utils');
+    config.resolve.alias['@interfaces'] = path.join(__dirname, 'interfaces');
+    config.resolve.alias['@styles'] = path.join(__dirname, 'styles');
+    config.resolve.alias['@containers'] = path.join(__dirname, 'containers');
+
+    return config;
+  },
+});
